4a79f28d44c31f4757273dab0d8acc2432931ae4
Devanshi-Sharma12 <sharmadevanshi608@gmail.com>
2024-07-26 01:36:46 +0530

    added

--- csvusingiterators.py
+++ csvusingiterators.py
@@ -1,24 +1,20 @@
 import csv
-from typing import List, Iterator
 
 class CSVIterator:
-    def __init__(self, file_path: str):
-            self.file = open(file_path, 'r', newline='')  
-            self.iterator = iter(self.file)  
+    def __init__(self, file_path):
+        self.file = open(file_path, 'r')
+        self.csv_reader = csv.reader(self.file)
 
-    def __iter__(self) -> Iterator[List[str]]:
+    def __iter__(self):
         return self
 
-    def __next__(self) -> List[str]:
+    def __next__(self):
         try:
-            line = next(self.iterator)  
-            row = line.strip().split(',') 
-            return row
+            return next(self.csv_reader)
         except:
             StopIteration
 
-# Usage example:
 csv_iter = CSVIterator('C:\\Users\\DELL\\OneDrive\\Desktop\\experiment\\most_wickets_t20_world_cup_2024.csv')
 
-row = next(csv_iter)
-print(row)
+for row in csv_iter:
+    print(row) 
