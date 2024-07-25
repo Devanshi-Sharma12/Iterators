3f161f36e030ffbd4df70043a2ad8ade30b7524c
Devanshi-Sharma12 <sharmadevanshi608@gmail.com>
2024-07-25 12:45:05 +0530

    updated

--- csvusingiterators.py
+++ csvusingiterators.py
@@ -1,20 +1,31 @@
-class CSVIterator:
-    def __init__(self, file_path):
-        self.file = open(file_path, 'r')
-        self.line = self.file.readline()
+import csv
+from typing import List, Iterator
 
-    def __iter__(self):
+class CSVIterator:
+    def __init__(self, file_path: str):
+            self.file = open(file_path, 'r', newline='')  
+            self.iterator = iter(self.file)  
+
+    def __iter__(self) -> Iterator[List[str]]:
         return self
 
-    def __next__(self):
-        if not self.line:
+    def __next__(self) -> List[str]:
+        if self.file is None:
             raise StopIteration
-        row = self.line.split(',')
-        self.line = self.file.readline()
-        return row
 
+        try:
+            line = next(self.iterator)  
+            row = line.strip().split(',') 
+            return row
+        except:
+            StopIteration
 
+    def __del__(self):
+        if self.file:
+            self.file.close()
+
+# Usage example:
 csv_iter = CSVIterator('C:\\Users\\DELL\\OneDrive\\Desktop\\experiment\\most_wickets_t20_world_cup_2024.csv')
 
-print(next(csv_iter)) #prints the consecutive row
-
+row = next(csv_iter)
+print(row)
